da86b1d0d9fca0433aacf0e35018885fb7d75e6b
Izzy <inovick@bu.edu>
2021-02-09 20:01:40 -0500

    Changed boxplot name

--- 01_Download.R
+++ 01_Download.R
@@ -5,5 +5,6 @@ install.packages("readr")
 library(readr)
 data <- read.csv("https://raw.githubusercontent.com/daviessw/BI586/master/R_Worksheet/Wasting_Data.csv", skip=1)
 library(ggplot2)
-boxplot(data$`Average.WD....`~Transect,data=data, main="not sure what to put here",
+boxplot(data$`Average.WD....`~Transect,data=data, main="Gitsandgiggles",
         xlab="Transect", ylab="Average wasting disease %")
+
