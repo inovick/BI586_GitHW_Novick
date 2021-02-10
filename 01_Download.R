3a54994aa6630e52f0616ba44c3fb0d7e6d5cbdd
Izzy <inovick@bu.edu>
2021-02-09 19:09:16 -0500

    Script created for gitHW, read in wasting_data.csv

---
+++ 01_Download.R
@@ -0,0 +1,6 @@
+#Isabel Novick github HW. Isabel=owner, Corinne=collaborator, 2/9/21
+
+
+install.packages("readr")
+library(readr)
+data <- read.csv("https://raw.githubusercontent.com/daviessw/BI586/master/R_Worksheet/Wasting_Data.csv", skip=1)
